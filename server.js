c524fe785a8d857bac28a592ad673f5538d4c2fe
Blake Alexander <blake.alexander.13@gmail.com>
2018-10-05 15:45:40 -0700

    fixed bugs

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 "use strict";
 
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+const express = require("express");
+const morgan = require("morgan");
 
-if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function() {
-    console.info(`App listening on ${this.address().port}`);
-  });
-}
+const myRouter = require("./myRouter");
+const app = express();
+
+app.use(morgan("common"));
+app.use(express.static("public"));
+app.use("/loadout", myRouter);
+// app.listen(process.env.PORT || 8080);
+
+// if (require.main === module) {
+app.listen(process.env.PORT || 8080, () => {
+  console.info(`App listening on port ${process.env.PORT || 8080}`);
+});
+// }
 
 module.exports = app;
