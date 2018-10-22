fe141ded69f9ee0a0ae354cd45ad92d943a52183
Blake Alexander <blake.alexander.13@gmail.com>
2018-10-22 13:49:23 -0700

    first attempt backend api

--- public/character.js
+++ public/character.js
@@ -1,11 +1,17 @@
-let list1 = document.getElementById("characterSelect");
+// let list1 = document.getElementById("characterSelect");
 
 function searchByUsername(searchTerm, callback) {
   var searchTerm = searchTerm.replace("#", "%23");
-  let bungieEP = `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/${searchTerm}/ `;
+  console.log("Hello from SearchByUsername!");
+
+  // $.getJSON(searchTerm, function(data) {
+  //   console.log(data);
+  // });
+
+  // let bungieEP = `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/${searchTerm}/ `;
 
   $.ajax({
-    url: bungieEP,
+    url: `/.bungieRouter`,
     type: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -19,7 +25,7 @@ function displayFromUsername(data) {
   console.log("displayFromUsername functioning");
   console.log(data);
   let destinyMembershipId = data.Response[0].membershipId;
-  searchByBungieId(destinyMembershipId, displayFromBungieId);
+  // searchByBungieId(destinyMembershipId, displayFromBungieId);
   // let name = data.Response;
   // let dropDown = {};
   // for(i=0;i<name.length;i++) {
@@ -40,61 +46,60 @@ function displayFromUsername(data) {
   // })
 }
 
-function searchByBungieId(membId, callback) {
-  let bungieEP = `https://www.bungie.net/Platform/User/GetMembershipsById/${membId}/0/`;
+// function searchByBungieId(membId, callback) {
+//   let bungieEP = `https://www.bungie.net/Platform/User/GetMembershipsById/${membId}/0/`;
 
-  $.ajax({
-    url: bungieEP,
-    type: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
-    },
-    success: callback
-  });
-}
+//   $.ajax({
+//     url: bungieEP,
+//     type: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
+//     },
+//     success: callback
+//   });
+// }
 
-function displayFromBungieId(info) {
-  console.log(info);
-  let membsId = info.Response.destinyMemberships[0].membershipId;
-  let membsType = info.Response.destinyMemberships[0].membershipType;
-  console.log(membsId);
-  searchByDestinyId(membsId, membsType, displayFromDestinyId);
-}
+// function displayFromBungieId(info) {
+//   console.log(info);
+//   let membsId = info.Response.destinyMemberships[0].membershipId;
+//   let membsType = info.Response.destinyMemberships[0].membershipType;
+//   console.log(membsId);
+//   searchByDestinyId(membsId, membsType, displayFromDestinyId);
+// }
 
-function searchByDestinyId(membsId, membsType, callback) {
-  let bungieEP = `https://www.bungie.net/Platform/Destiny2/${membsType}/Account/${membsId}/Stats/?components=205`;
+// function searchByDestinyId(membsId, membsType, callback) {
+//   let bungieEP = `https://www.bungie.net/Platform/Destiny2/${membsType}/Account/${membsId}/Stats/?components=205`;
 
-  $.ajax({
-    url: bungieEP,
-    type: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
-    },
-    success: callback,
-    error: console.log("This is an old account!")
-  });
-}
+//   $.ajax({
+//     url: bungieEP,
+//     type: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
+//     },
+//     success: callback
+//   });
+// }
 
-function displayFromDestinyId(data) {
-  console.log("displayFromDestinyId functioning");
-  console.log(data);
-  let character = data.Response.characters;
-  let mergedAll = data.Response.mergedAllCharacters;
-  let mergedAllTime = mergedAll.results.allPvP.allTime;
-  console.log(character);
-  console.log(mergedAll);
-  for (i = 0; i < character.length; i++) {
-    $(".js-search-results").append(`<p>${character[i].characterId}</p>`);
-  }
-  $(".js-search-results2").html(`<p>Total Kills: ${
-    mergedAllTime.kills.basic.displayValue
-  }</p><br>
-  <p>Total Deaths: ${mergedAllTime.deaths.basic.displayValue}</p><br>
-  <p>K/D ratio: ${mergedAllTime.killsDeathsRatio.basic.displayValue}</p><br>
-  <p>KA/D ratio: ${mergedAllTime.killsDeathsAssists.basic.displayValue}</p>`);
-}
+// function displayFromDestinyId(data) {
+//   console.log("displayFromDestinyId functioning");
+//   console.log(data);
+//   let character = data.Response.characters;
+//   let mergedAll = data.Response.mergedAllCharacters;
+//   let mergedAllTime = mergedAll.results.allPvP.allTime;
+//   console.log(character);
+//   console.log(mergedAll);
+//   for (i = 0; i < character.length; i++) {
+//     $(".js-search-results").append(`<p>${character[i].characterId}</p>`);
+//   }
+//   $(".js-search-results2").html(`<p>Total Kills: ${
+//     mergedAllTime.kills.basic.displayValue
+//   }</p><br>
+//   <p>Total Deaths: ${mergedAllTime.deaths.basic.displayValue}</p><br>
+//   <p>K/D ratio: ${mergedAllTime.killsDeathsRatio.basic.displayValue}</p><br>
+//   <p>KA/D ratio: ${mergedAllTime.killsDeathsAssists.basic.displayValue}</p>`);
+// }
 
 function watchSubmit() {
   $(".js-search-form").submit(event => {
@@ -108,36 +113,36 @@ function watchSubmit() {
 
 $(watchSubmit);
 
-// $('.js-search-results').append(`<div class="accountSelector"><p>${name[i].displayName}</p></div>`);
-//     if (name[i].hasOwnProperty('blizzardDisplayName')) {
-//       $('.js-search-results').append(`<p class="otherIDs">-Blizzard ID: ${name[i].blizzardDisplayName}</p>`);
-//     }
-//     if (name[i].hasOwnProperty('xboxDisplayName')) {
-//       $('.js-search-results').append(`<p class="otherIDs">-Xbox Gamertag: ${name[i].xboxDisplayName}</p>`);
-//     }
-//     if (name[i].hasOwnProperty('psnDisplayName')) {
-//       $('.js-search-results').append(`<p class="otherIDs">-PSN name: ${name[i].psnDisplayName}</p>`);
-//     }
+// // $('.js-search-results').append(`<div class="accountSelector"><p>${name[i].displayName}</p></div>`);
+// //     if (name[i].hasOwnProperty('blizzardDisplayName')) {
+// //       $('.js-search-results').append(`<p class="otherIDs">-Blizzard ID: ${name[i].blizzardDisplayName}</p>`);
+// //     }
+// //     if (name[i].hasOwnProperty('xboxDisplayName')) {
+// //       $('.js-search-results').append(`<p class="otherIDs">-Xbox Gamertag: ${name[i].xboxDisplayName}</p>`);
+// //     }
+// //     if (name[i].hasOwnProperty('psnDisplayName')) {
+// //       $('.js-search-results').append(`<p class="otherIDs">-PSN name: ${name[i].psnDisplayName}</p>`);
+// //     }
 
-// switch (name[i].blizzardDisplayName || name[i].xboxDisplayName || name[i].psnDisplayName) {
-//   case name[i].displayName:
-//     $('.js-search-results').append(`<div class="accountSelector">
-//     <p>${name[i].displayName}</p></div>`);
-//   case name[i].blizzardDisplayName:
-//     $('.accountSelector').append(`<p class="otherIDs">Blizzard ID: ${name[i].blizzardDisplayName}</p>`);
-//   case name[i].xboxDisplayName:
-//     $('.accountSelector').append(`<p class="otherIDs">Xbox Gamertag: ${name[i].xboxDisplayName}</p>`);
-//   case name[i].psnDisplayName:
-//     $('.accountSelector').append(`<p class="otherIDs">PSN name: ${name[i].psnDisplayName}</p>`);
-//   default:
-//     break;
-//   }
-// if(name[i].blizzardDisplayName) {
-//   $('.accountSelector').append(`<p class="otherIDs">Blizzard ID: ${name[i].blizzardDisplayName}</p>`);
-// }
-// else if(name[i].xboxDisplayName) {
-//   $('.accountSelector').append(`<p class="otherIDs">Xbox Gamertag: ${name[i].xboxDisplayName}</p>`);
-// }
-// else if(name[i].psnDisplayName) {
-//   $('.accountSelector').append(`<p class="otherIDs">PSN name: ${name[i].psnDisplayName}</p>`);
-// }
+// // switch (name[i].blizzardDisplayName || name[i].xboxDisplayName || name[i].psnDisplayName) {
+// //   case name[i].displayName:
+// //     $('.js-search-results').append(`<div class="accountSelector">
+// //     <p>${name[i].displayName}</p></div>`);
+// //   case name[i].blizzardDisplayName:
+// //     $('.accountSelector').append(`<p class="otherIDs">Blizzard ID: ${name[i].blizzardDisplayName}</p>`);
+// //   case name[i].xboxDisplayName:
+// //     $('.accountSelector').append(`<p class="otherIDs">Xbox Gamertag: ${name[i].xboxDisplayName}</p>`);
+// //   case name[i].psnDisplayName:
+// //     $('.accountSelector').append(`<p class="otherIDs">PSN name: ${name[i].psnDisplayName}</p>`);
+// //   default:
+// //     break;
+// //   }
+// // if(name[i].blizzardDisplayName) {
+// //   $('.accountSelector').append(`<p class="otherIDs">Blizzard ID: ${name[i].blizzardDisplayName}</p>`);
+// // }
+// // else if(name[i].xboxDisplayName) {
+// //   $('.accountSelector').append(`<p class="otherIDs">Xbox Gamertag: ${name[i].xboxDisplayName}</p>`);
+// // }
+// // else if(name[i].psnDisplayName) {
+// //   $('.accountSelector').append(`<p class="otherIDs">PSN name: ${name[i].psnDisplayName}</p>`);
+// // }
